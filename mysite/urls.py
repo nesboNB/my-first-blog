5b8ac15cae4a406f9c00903e0520d1a14bf0e3db
nesbo <nesbo@example.ch>
2024-03-26 14:00:50 +0100

    Endlich

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,10 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('', include('blog.urls'))
 ]
